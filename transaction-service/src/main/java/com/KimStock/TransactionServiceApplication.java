4f4ee6c17d1500928b5ad8312a5dc9371da6f74a
SeungSu <97881804+KoKimSS@users.noreply.github.com>
2025-04-21 23:18:20 +0900

    feat: 메인 함수에 @Import(EnvConfig.class) 추가

--- transaction-service/src/main/java/com/KimStock/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/KimStock/TransactionServiceApplication.java
@@ -1,9 +1,12 @@
 package com.KimStock;
 
+import com.common.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(EnvConfig.class)
 public class TransactionServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransactionServiceApplication.class, args);
