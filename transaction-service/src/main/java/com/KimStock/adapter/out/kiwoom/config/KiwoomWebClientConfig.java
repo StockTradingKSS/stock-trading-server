8ace65a740bf0e122d759d7142f9d7ad2a8c1da1
SeungSu <97881804+KoKimSS@users.noreply.github.com>
2025-04-26 19:59:41 +0900

    feat: kiwoom 증권 전용 WebClient Config 추가

---
+++ transaction-service/src/main/java/com/KimStock/adapter/out/kiwoom/config/KiwoomWebClientConfig.java
@@ -0,0 +1,54 @@
+package com.KimStock.adapter.out.kiwoom.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
+
+@Configuration
+public class KiwoomWebClientConfig {
+
+    @Value("${kiwoom.api.base-url}")
+    private String baseUrl;
+
+    @Bean( name = "kiwoomWebClient")
+    public WebClient kiwoomInvestmentWebClient() {
+        // 메모리 제한 설정 확장 (기본값: 256KB)
+        final int size = 16 * 1024 * 1024; // 16MB
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
+
+        // Connection Pool 설정
+        ConnectionProvider connectionProvider = ConnectionProvider.builder("kiwoom-investment-connection-pool")
+                .maxConnections(500)
+                .maxIdleTime(Duration.ofSeconds(60))
+                .maxLifeTime(Duration.ofMinutes(10))
+                .pendingAcquireTimeout(Duration.ofSeconds(60))
+                .evictInBackground(Duration.ofSeconds(120))
+                .build();
+
+        // HTTP Client 설정
+        HttpClient httpClient = HttpClient.create(connectionProvider)
+                .responseTimeout(Duration.ofSeconds(30));
+
+        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+
+        return WebClient.builder()
+                .clientConnector(connector)
+                .baseUrl(baseUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("charset", "UTF-8")
+                .exchangeStrategies(strategies)
+                .build();
+    }
+}
