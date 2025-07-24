e0b28b69072a762dc5ffd8412f3bf21b351f6d77
SeungSu <seungsu8848@ajou.ac.kr>
2025-07-24 23:13:33 +0900

    chore: Docker 파일 수정 및 .env -> secretkey.properties 로 환경변수 import 변경

--- stock-service/src/main/java/com/KimStock/TransactionServiceApplication.java
+++ stock-service/src/main/java/com/KimStock/TransactionServiceApplication.java
@@ -1,18 +1,13 @@
 package com.KimStock;
 
-import com.common.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@Import(EnvConfig.class)
 public class TransactionServiceApplication {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(TransactionServiceApplication.class);
-        app.addInitializers(new EnvConfig.DotenvInitializer());
-        app.run(args);
+        SpringApplication.run(TransactionServiceApplication.class, args);
     }
 }
