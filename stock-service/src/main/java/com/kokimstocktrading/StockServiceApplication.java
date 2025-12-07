c07153ed320f16c9623806b872132c469a4ad728
KoKImSS <97881804+KoKimSS@users.noreply.github.com>
2025-12-07 14:08:00 +0900

    refactor: 메인 클래스 이름 변경

--- stock-service/src/main/java/com/kokimstocktrading/StockServiceApplication.java
+++ stock-service/src/main/java/com/kokimstocktrading/StockServiceApplication.java
@@ -6,9 +6,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class TransactionServiceApplication {
+public class StockServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(TransactionServiceApplication.class, args);
+    SpringApplication.run(StockServiceApplication.class, args);
   }
 }
